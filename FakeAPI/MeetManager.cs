122e643faa2b7b7e88cb55f8a46e0a783bb41067
TitikUniverse <semyon.titov.203@mail.ru>
2021-12-23 19:22:38 +0300

    Initial commit

---
+++ FakeAPI/MeetManager.cs
@@ -0,0 +1,18 @@
+﻿using FakeAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeAPI
+{
+    public class MeetManager
+    {
+        List<Meet> meets = new List<Meet>();
+
+        public void AddNewMeet(string name, DateTime meetTime)
+        {
+            Meet newMeet = new Meet() { Name = name, CreateTime = DateTime.Now, MeetTime = meetTime };
+            meets.Add(newMeet);
+        }
+    }
+}
---
+++ FakeAPI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FakeAPI
+{
+    class Program
+    {
+        private static MeetManager meetManager;
+
+        static void Main(string[] args)
+        {
+            // Создал нового менеджера встречь
+            meetManager = new MeetManager();
+
+            Console.Write("Введите имя встречи: ");
+            string user_nameMeet = Console.ReadLine();
+
+            Console.Write("Введите год встречи: ");
+            int user_meetTimeYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите месяц встречи: ");
+            int user_meetTimeMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите день встречи: ");
+            int user_meetTimeDay = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите час встречи: ");
+            int user_meetTimeHour = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите минуту встречи: ");
+            int user_meetTimeMinute = int.Parse(Console.ReadLine());
+
+            DateTime user_meetTime = new DateTime(
+                user_meetTimeYear, 
+                user_meetTimeMonth, 
+                user_meetTimeDay, 
+                user_meetTimeHour, 
+                user_meetTimeMinute, 
+                0
+            );
+
+            meetManager.AddNewMeet(user_nameMeet, user_meetTime);
+
+            Console.ReadKey();
+        }
+    }
+}
